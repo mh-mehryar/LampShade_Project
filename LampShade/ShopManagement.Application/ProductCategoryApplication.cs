ae6fe7045d9d7999b61e74a248252bd94aec9fc6
Mohammad Hossein <mh.mehryar@gmail.com>
2022-04-26 11:01:43 +0430

    14010206

---
+++ LampShade/ShopManagement.Application/ProductCategoryApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Application
+{
+    public class ProductCategoryApplication : IProductCategoryApplication
+    {
+        public void Create(CreateProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(EditProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
